798e397fc146375af6b84ebecd06ad5ce03e64bf
Trojanforyou <necro_sokolov@mail.ru>
2025-12-22 15:17:32 +0100

    updatedMinimap

--- utils/parsing_utils.c
+++ utils/parsing_utils.c
@@ -6,7 +6,7 @@
 /*   By: msokolov <msokolov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/22 14:38:22 by msokolov          #+#    #+#             */
-/*   Updated: 2025/12/22 14:43:55 by msokolov         ###   ########.fr       */
+/*   Updated: 2025/12/22 15:16:04 by msokolov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,4 +34,32 @@ char **set_map(char *line, int fd, t_data *data, char **temp, int *i)
 		(*i)++;
 	}
 	return(temp);
+}
+bool	set_collor(t_data *data, int y, int x)
+{
+	int	color;
+
+	color = 0;
+    while (data->map[y])
+    {
+		x = 0;
+        while (data->map[y][x])
+        {
+            if (data->map[y][x] == '0')
+                color = 0x654321FF; // Hex (RGBA / 0xRRGGBBAA):
+            else if (data->map[y][x] == '1')
+                color = 0x000000FF; // Hex (RGBA / 0xRRGGBBAA):
+            else if (data->map[y][x] == 'P')
+                color = 0xFF0000FF; // Hex (RGBA / 0xRRGGBBAA):
+            else
+            {
+                (x)++;
+                continue;
+            }
+            put_pixel(data, x, y, color);
+            x++;
+        }
+        y++;
+	}
+	return(true);
 }
